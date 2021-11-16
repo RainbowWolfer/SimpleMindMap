101e9f117b972308aff78ee218c6cbadd48ac053
RainbowWolfer <RainbowWolfer@outlook.com>
2021-11-16 16:02:08 +0800

    Regular Dev

---
+++ Entities/Vector2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MindMap.Entities {
+	public class Vector2 {
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public static readonly Vector2 Zero = new();
+		public static readonly Vector2 One = new(1, 1);
+
+		public Vector2() {
+			X = 0;
+			Y = 0;
+		}
+
+		public Vector2(double x, double y) {
+			this.X = x;
+			this.Y = y;
+		}
+
+		public Vector2(Point point) {
+			this.X = point.X;
+			this.Y = point.Y;
+		}
+
+		public Vector2(Vector vector) {
+			this.X = vector.X;
+			this.Y = vector.Y;
+		}
+
+		public Vector2 Clone() => new Vector2(X, Y);
+
+		public Vector2 Bound(Vector2 from, Vector2 to) {
+			var result = this.Clone();
+			if(result.X < from.X) {
+				result.X = from.X;
+			} else if(result.X > to.X) {
+				result.X = to.X;
+			}
+			if(result.Y < from.Y) {
+				result.Y = from.Y;
+			} else if(result.Y > to.Y) {
+				result.Y = to.Y;
+			}
+			return result;
+		}
+
+		public override string ToString() {
+			return $"Vector2: ({X},{Y})";
+		}
+
+		public override bool Equals(object? obj) {
+			return obj is Vector2 v && v.X == X && v.Y == Y;
+		}
+
+		public override int GetHashCode() {
+			return base.GetHashCode();
+		}
+
+
+		public static implicit operator Vector2(Point point) {
+			return new Vector2(point.X, point.Y);
+		}
+
+		public static implicit operator Vector2(Vector vec) {
+			return new Vector2(vec.X, vec.Y);
+		}
+
+		public static implicit operator string(Vector2 vec) => vec.ToString();
+
+		public static bool operator ==(Vector2 v1, Vector2 v2) => v1.Equals(v2);
+		public static bool operator !=(Vector2 v1, Vector2 v2) => !v1.Equals(v2);
+
+		public static Vector2 operator +(Vector2 v1, Vector2 v2) {
+			return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+		}
+		public static Vector2 operator -(Vector2 v1, Vector2 v2) {
+			return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
+		}
+		public static Vector2 operator -(Vector2 v) {
+			return new Vector2(-v.X, -v.Y);
+		}
+	}
+}
--- Pages/MindMapPage.xaml.cs
+++ Pages/MindMapPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MindMap.Entities;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +17,63 @@ using System.Windows.Shapes;
 
 namespace MindMap.Pages {
 	public partial class MindMapPage: Page {
+
 		public MindMapPage() {
 			InitializeComponent();
+
+			MainCanvas.MouseMove += Rect_MouseMove;
+			MainCanvas.MouseUp += Rect_MouseUp;
+
+			DebugMousePosition();
+		}
+
+		private async void DebugMousePosition() {
+			while(true) {
+				CurrentMousePositionText.Text = (Vector2)Mouse.GetPosition(MainCanvas);
+				await Task.Delay(10);
+			}
 		}
 
 		private void AddRectableButton_Click(object sender, RoutedEventArgs e) {
-			
+			Rectangle rect = new() {
+				Width = 50,
+				Height = 50,
+				Fill = new SolidColorBrush(Colors.Red),
+				RenderTransform = new TranslateTransform(0, 0),
+			};
+			rect.SetValue(Canvas.TopProperty, 0.0);
+			rect.SetValue(Canvas.LeftProperty, 0.0);
+			rect.MouseDown += Rect_MouseDown;
+			MainCanvas.Children.Add(rect);
 		}
+
+
+		private bool _drag;
+		private Vector2 offset;
+		private void Rect_MouseMove(object sender, MouseEventArgs e) {
+			if(!_drag) {
+				return;
+			}
+			Rectangle? found = MainCanvas.Children.Cast<Shape>().FirstOrDefault(s => s is Rectangle) as Rectangle;
+
+			Vector2 mouse_position = e.GetPosition(MainCanvas);
+
+			Canvas.SetLeft(found, mouse_position.X);
+			Canvas.SetTop(found, mouse_position.Y);
+
+			CurrentPositionText.Text = new Vector2(Canvas.GetLeft(found), Canvas.GetTop(found));
+		}
+
+		private void Rect_MouseDown(object sender, MouseButtonEventArgs e) {
+			_drag = true;
+			//offset=
+			Mouse.Capture(sender as UIElement);
+		}
+
+		private void Rect_MouseUp(object sender, MouseButtonEventArgs e) {
+			_drag = false;
+			Mouse.Capture(null);
+		}
+
 	}
 }
